17cda6fb4784cdb5b1dd8e78b332c20386c62b3f
Flyingmana <flyingmana@googlemail.com>
2023-01-20 21:26:38 +0100

    add ability to use markdown

--- conf.py
+++ conf.py
@@ -31,6 +31,7 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
